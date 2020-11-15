2f990c0a345b9b2cae08104b689a867ff749bc2c
Curtis <curtissmp@gmail.com>
2020-11-14 21:12:54 -0500

    Finished Voronoi Tess and Perfromance

--- NearestNeighbors.py
+++ NearestNeighbors.py
@@ -1,192 +1,163 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import random
-import timeit
+import time
 from scipy.spatial import Voronoi, voronoi_plot_2d, KDTree, Delaunay, delaunay_plot_2d
 # from sklearn.neighbors import NearestNeighbors
 
+##########################################################################
+##### Global variables
+# Index to test getting neighbors on
+indexToGetNeighbors = 0
 
+# Radius to get neighbors (fixed Distance)
+radiusParam = 50
 
-### Random points
+# Number of points to get for testing
+numOfPointsToGen = 50
+
+# Random points ranges
+xDataRangeLower, xDataRangeUpper = 0, 200
+yDataRangeLower, yDataRangeUpper = 0, 200
+
+# Range and domain of graph to output
+xLowerLimit = -5
+xHigherLimit = 250
+yLowerLimit = -5
+yHigherLimit = 250
+
+##### Global variables
+##########################################################################
+
+##########################################################################
+### Random points for testing the functions
 def getRandomPoints():
-    radius = 20
-    dataRangeX = (0, 250)
-    dataRangeY = (0, 250)
-    ###numOfPoints hits limit at 117; 118+ does not process for some reason...
-    numOfPoints = 10
-
-    deltas = set()
-    for x in range(-radius, radius + 1):
-        for y in range(-radius, radius + 1):
-            if x*x + y*y <= radius*radius:
-                deltas.add((x,y))
-
-    randPoints = []
-    excluded = set()
-    i = 0
-    while i < numOfPoints:
-        x = random.randrange(*dataRangeX)
-        y = random.randrange(*dataRangeY)
-        if (x,y) in excluded:
-            continue
-        randPoints.append((x,y))
-        i += 1
-        excluded.update((x+dx, y+dy) for (dx,dy) in deltas)
     
+    coords = [( round(random.random() * xDataRangeUpper), round(random.random() * yDataRangeUpper) )
+              for _ in range(numOfPointsToGen)]
+    randPoints = coords
+    
+    # Points further out to fix infinite Voronoi edges (coloring)
+    randPoints = np.append(randPoints, [[999,999], [-999,999], [999,-999], [-999,-999]], axis = 0)
     return randPoints
 ### end RandomPoints
+##########################################################################
 
-# Gets Nearest Neighbors based on circular radius/range from point
+##########################################################################
+# Fixed Distance: Gets Nearest Neighbors based on circular radius/range from point
 def fixedDistanceNN(npPoints):
     
     vor = Voronoi(npPoints)
-
-    fig = voronoi_plot_2d(vor, show_vertices=False)
-
-    ax = fig.add_subplot(1,1,1)
+    _ = voronoi_plot_2d(vor, show_vertices=False)
     
+    # Annotate the points
     for i in range(0, len(npPoints)):
-        ax.annotate(text = i, xy = (npPoints[i][0], npPoints[i][1]))
+        plt.annotate(text = i, xy = (npPoints[i][0], npPoints[i][1]))
     
-    circ = plt.Circle(npPoints[0], radius=100, fill=False, edgecolor='green')
-    ax.add_patch(circ)
+    # Plot the radius used to get the neighbors
+    circle1 = plt.Circle(npPoints[0], radiusParam, ec='g', fc='w')
+    plt.gcf().gca().add_artist(circle1)
 
-    print('Fixed Length Radius: ')
-    print(KDTree(npPoints).query_ball_point(npPoints[0], 100))
+    # Gets neighbors at indicated index for specified range
+    # --> Used KDTree
+    print('Fixed Length Radius Neighbors: ', 
+          KDTree(npPoints).query_ball_point(npPoints[indexToGetNeighbors], radiusParam))
 
+    # Change and print the plot to a square equal axis
+    plt.axis('equal')
+    plt.xlim([xLowerLimit, xHigherLimit]), plt.ylim([yLowerLimit, yHigherLimit])
     plt.savefig('graph_FixedDistance.png')
     
     return
+### end fixedDistance
+##########################################################################
 
-
+##########################################################################
+# Helper function: Delaunay graph, for Voronoi cell neighbor coloring
 def delaunayGraph(npPoints):
-    delaunay = Delaunay(npPoints, qhull_options='Qc')
+    
+    # Use delaunay triangulation graph to get neighbors
+    delaunay = Delaunay(npPoints[:-4], qhull_options='Qc')
+    _ = delaunay_plot_2d(delaunay)
+  
+    # Annotate the points  
+    for i in range(0, len(npPoints[:-4])):
+        plt.annotate(text = i, xy = (npPoints[:-4][i][0], npPoints[:-4][i][1]))
 
-    fig = delaunay_plot_2d(delaunay)
-
-    ax = fig.add_subplot(1,1,1)
-    
-    for i in range(0, len(npPoints)):
-        ax.annotate(text = i, xy = (npPoints[i][0], npPoints[i][1]))
-    
-    plt.savefig('graph_Delaunay.png')
-    
-    indptr = delaunay.vertex_neighbor_vertices[0]
-    indices = delaunay.vertex_neighbor_vertices[1]
+    # Use delaunay triangulation to get the neighbors
+    indptr, indices = delaunay.vertex_neighbor_vertices
     # ([indptr], [indices])
     # for index k, neighbors are indices[indptr[k]:indptr[k+1]]
     delaunayDict = dict()
-    for k in range(0, len(npPoints)):
+    for k in range(0, len(npPoints[:-4])):
         startIndex = indptr[k]
         endIndex = indptr[k + 1]
-        # print(k, ": ")
         for i in range(startIndex, endIndex):
-            # print(indices[i])
             if k in delaunayDict:
                 delaunayDict[k].append(indices[i])
             else:
                 delaunayDict[k] = [ indices[i] ]
+                
+    # Change and print the plot to a square equal axis
+    plt.axis('equal')
+    plt.xlim([xLowerLimit, xHigherLimit]), plt.ylim([yLowerLimit, yHigherLimit])
+    plt.savefig('graph_Delaunay.png')
 
     return delaunayDict
 
-# Gets neighbors connecting to voronoi cell
+# Voronoi Tessellation: Gets neighbors connecting to voronoi cell
 def voronoiTessellation(npPoints):
-    #print(KDTree(npPoints).query_pairs(r=150))
+    
+    # Use delaunay triangulation graph to get neighbors
     delaunayDict = delaunayGraph(npPoints)
-    
-    #print(delaunayDict)
-    
     vor = Voronoi(npPoints, qhull_options='Qc')
-
-    fig = voronoi_plot_2d(vor, show_vertices=False)
-
-    ax = fig.add_subplot(1,1,1)
-
+    _ = voronoi_plot_2d(vor, show_vertices=False)
     
+    # Annotate the points
     for i in range(0, len(npPoints)):
-        ax.annotate(text = i, xy = (npPoints[i][0], npPoints[i][1]))
-
-    # For each vertex
-    for entry in delaunayDict:
-        # print(entry, delaunayDict[entry])
-        polygon = [ vor.vertices[i] for neighbor in delaunayDict[entry] ]
-        plt.fill(*zip(*polygon))
-
-    print('Voronoi Tesselation Neighbors: ' + delaunayDict[0])
-    for indx in delaunayDict[0]:
-        #print(vor.point_region[indx])
-        region = vor.regions[vor.point_region[indx]]
+        plt.annotate(text = i, xy = (npPoints[i][0], npPoints[i][1]))
+    
+    # Color neighbors to 'indexToGetNeighbors'
+    for indx in delaunayDict[indexToGetNeighbors]:
+        region = vor.regions[ vor.point_region[indx] ]
         if not -1 in region:
             polygon = [vor.vertices[i] for i in region]
             plt.fill(*zip(*polygon))
     
+    # Change and print the plot to a square equal axis
+    plt.axis('equal')
+    plt.xlim([xLowerLimit, xHigherLimit]), plt.ylim([yLowerLimit, yHigherLimit])
     plt.savefig('graph_VoronoiTessellation.png',)
     
+    print('Voronoi Tesselation Neighbors: ', delaunayDict[indexToGetNeighbors])
+    
     return
+### end voronoi tessellation and helper function
+##########################################################################
 
-
+##########################################################################
+# Performance: Measures timing performance between fixedDistance and Voronoi functions
 def performance():
+    npPoints = np.array(getRandomPoints())
     
-    mySetup = '''
-        import numpy as np
-        import matplotlib.pyplot as plt
-        import random
-        from scipy.spatial import Voronoi, voronoi_plot_2d, KDTree, Delaunay, delaunay_plot_2d
-
-
-    ### Random points
-    radius = 20
-    dataRangeX = (0, 250)
-    dataRangeY = (0, 250)
-    ###numOfPoints hits limit at 117; 118+ does not process for some reason...
-    numOfPoints = 117
-
-    deltas = set()
-    for x in range(-radius, radius + 1):
-        for y in range(-radius, radius + 1):
-            if x*x + y*y <= radius*radius:
-                deltas.add((x,y))
-
-    randPoints = []
-    excluded = set()
-    i = 0
-    while i < numOfPoints:
-        x = random.randrange(*dataRangeX)
-        y = random.randrange(*dataRangeY)
-        if (x,y) in excluded:
-            continue
-        randPoints.append((x,y))
-        i += 1
-        excluded.update((x+dx, y+dy) for (dx,dy) in deltas)
-    ### end RandomPoints
-    '''
-    fixedDistanceRadius = '''
-        vor = Voronoi(npPoints)
-
-        fig = voronoi_plot_2d(vor, show_vertices=False)
-
-    ax = fig.add_subplot(1,1,1)
+    #Fixed Distance
+    startFD = time.time()
+    fixedDistanceNN(npPoints)
+    endFD = time.time()
+    fixedDistanceTime = (endFD - startFD)
     
-    for i in range(0, len(npPoints)):
-        ax.annotate(text = i, xy = (npPoints[i][0], npPoints[i][1]))
+    #Voronoi Tessellation
+    startVT = time.time()
+    voronoiTessellation(npPoints)
+    endVT = time.time()
+    voronoiTessellationTime = (endVT - startVT)
     
-    circ = plt.Circle(npPoints[0], radius=100, fill=False, edgecolor='green')
-    ax.add_patch(circ)
+    print("Time to execute FixedDistance : ", fixedDistanceTime, " seconds")
+    print("Time to execute VoronoiTess:    ", voronoiTessellationTime, " seconds")
+    
+### end performance
+##########################################################################
 
-    KDTree(npPoints).query_ball_point(npPoints[0], 100)
-    '''
-
-    #timeit statement
-    print('Fixed Distance Performance Analysis: ')
-    print (timeit.timeit(setup = mySetup, stmt = fixedDistanceRadius, number = 10000))
-    return
-
-
-
-
-# Call functions
-npPoints = np.array(getRandomPoints())
-fixedDistanceNN(npPoints)
+##########################################################################
+# Main: Call functions
 performance()
-#voronoiTessellation(npPoints)
-#delaunayGraph(npPoints)
